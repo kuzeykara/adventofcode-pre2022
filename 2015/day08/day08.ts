243fc90dd4e018410e3c623de00080ca11daf5ab
Kuzey Kara <kuzeykara07@gmail.com>
2022-12-08 21:36:23 +0100

    solved part 2

--- 2015/day08/day08.ts
+++ 2015/day08/day08.ts
@@ -56,8 +56,39 @@ function part01() {
 }
 
 function part02() {
+    function strEncodedLength(inputStr: string): number {
+        // +2 since we're adding double quotes to each strings head and tail
+        let encodedLength: number = inputStr.length+2; 
 
+        for (let i=0; i<inputStr.length; i++) {
+            let currChar = inputStr.charAt(i);
+            if (currChar === '"') {
+                encodedLength++;
+            } else if (currChar === '\\') {
+                encodedLength++;
+            }
+        }
+
+        return encodedLength;
+    }
+
+    function calcMemoryDifferences(inputs: string[]): number {
+        let originalChars: number = 0;
+        let numInCode: number = 0;
+
+        inputs.forEach( (str: string) => {
+            originalChars += str.length;
+            numInCode += strEncodedLength(str);
+        } );
+
+        return numInCode-originalChars;
+    }
+
+    const inputStrings: string[] = input.split('\n');
+    const result: number = calcMemoryDifferences(inputStrings);
+
+    console.log(result);
 }
 
-part01();
-//part02();
+//part01();
+part02();
